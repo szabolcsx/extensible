4d1077e000adac99d009bf12cf4cf80687a4239b
Szőke Szabolcs <szabolcsx95@gmail.com>
2015-11-25 11:11:02 +0200

    New include file which includes all headers

---
+++ include/extensible.h
@@ -0,0 +1,12 @@
+//
+// Created by szoke on 2015.11.25..
+//
+
+#ifndef EXTENSIBLE_EXTENSIBLE_H
+#define EXTENSIBLE_EXTENSIBLE_H
+
+#include <iextension.h>
+#include <manager.h>
+#include <extension.h>
+
+#endif //EXTENSIBLE_EXTENSIBLE_H
